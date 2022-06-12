6a88467f882e5bf491f10ddf12fa0014345bb61d
Alyssa Briscoe <103342539+alyssa-briscoe@users.noreply.github.com>
2022-06-12 10:53:37 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const myModal = document.getElementById('myModal');
-const myInput = document.getElementById('myInput');
+const myModal = document.getElementById('exampleModal');
+const myInput = document.getElementById('twitch-embed');
 
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus();
 })
+
+const modal = document.getElementById('exampleModal');
+const sound = document.getElementById('twitch-embed');
+
+modal.addEventListener("hide.bs.modal", () => {
+    sound.remove();
+    document.location.reload(true);
+});
 
 function remove (e) {
     
