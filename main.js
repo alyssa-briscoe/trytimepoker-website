7f73461d3046e76f35a9c792474849ba30b33d54
Alyssa Briscoe <103342539+alyssa-briscoe@users.noreply.github.com>
2022-06-12 10:16:32 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -13,17 +13,4 @@ function remove (e) {
    
 }
 
-function addDiv () {
-    
-    const newDiv = document.createElement("div");
-    const before = document.getElementById("before_twitch");
-    const set = newDiv.setAttribute('id', 'twitch-embed');
-    const append = set.appendChild(before);
-    const script = document.createElement("script");
-    script.src="https://player.twitch.tv/js/embed/v1.js";
-    append.appendChild(script);
-
-    
-    
-}
     
