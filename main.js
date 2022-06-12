e04bd3db50bfe99927ada115a2287418822fa87a
Alyssa Briscoe <103342539+alyssa-briscoe@users.noreply.github.com>
2022-06-11 21:58:19 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
-const scrollingElement = (document.scrollingElement || document.body);
-scrollingElement.scrollTop = scrollingElement.scrollHeight;
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus();
+})
+
+function remove (e) {
+    
+    const sound = document.getElementById('twitch-embed');
+    sound.remove();
+    document.location.reload(true);
+   
+}
+
+function addDiv () {
+    
+    const newDiv = document.createElement("div");
+    const before = document.getElementById("before_twitch");
+    const set = newDiv.setAttribute('id', 'twitch-embed');
+    const append = set.appendChild(before);
+    const script = document.createElement("script");
+    script.src="https://player.twitch.tv/js/embed/v1.js";
+    append.appendChild(script);
+
+    
+    
+}
+    
